2eb0b54dce99df3f24a84ea98ebbd9a46509fc35
hnnyzyf <hnnyzyf2013@163.com>
2018-04-04 22:09:51 +0800

    add server package

---
+++ src/util/server/server.go
@@ -0,0 +1,68 @@
+package server
+
+import meta "util/meta"
+import "bytes"
+import "fmt"
+
+const (
+	_ = iota
+	Task_relation
+	Task_index
+)
+
+type Server interface {
+	//创建一个新的连接
+	CreateSession2DB(string) string
+	//Server负责查询
+	QueryTable(string, string) (*meta.Meta, error)
+	QueryTrue0False(string, string, int) (bool, error)
+	QuerySelectivity(string, string) (int, error)
+	QueryIndex(string, string) (*meta.Index, error)
+	QueryRow(string, string) (int, error)
+	QueryCardinality(string, string) (int, error)
+}
+
+//Server的实现
+type Mysql struct {
+	User     string
+	Password string
+	Host     string
+	Port     string
+	DB       string
+}
+
+func NewMysqlServer(u string, p string, h string, o string, d string) *Mysql {
+	return &Mysql{
+		User:     u,
+		Password: p,
+		Host:     h,
+		Port:     o,
+		DB:       d,
+	}
+}
+
+//生成连接字符串
+func (m *Mysql) CreateConnectionString(DB string) string {
+	buf := bytes.NewBuffer([]byte{})
+	fmt.Fprint(buf, m.User)
+	fmt.Fprint(buf, ":")
+	fmt.Fprint(buf, m.Password)
+	fmt.Fprint(buf, "@tcp(")
+	fmt.Fprint(buf, m.Host)
+	fmt.Fprint(buf, ":")
+	fmt.Fprint(buf, m.Port)
+	fmt.Fprint(buf, ")/")
+	fmt.Fprint(buf, DB)
+	return buf.String()
+}
+
+func (m *Mysql) CreateSession2DB(DB string) string {
+	switch DB {
+	case m.DB:
+		return m.CreateConnectionString(m.DB)
+	case "":
+		return m.CreateConnectionString(m.DB)
+	default:
+		return m.CreateConnectionString(DB)
+	}
+}
