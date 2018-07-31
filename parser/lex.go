64ffdd41ab3ccfe0280a13aea8ce8e6ade8ddd8c
hnnyzyf <hnnyzyf2013@163.com>
2018-07-31 22:43:39 +0800

    add hack

---
+++ parser/lex.go
@@ -0,0 +1,13 @@
+package parser
+
+//this section give the lex defination 
+//
+//letter [a-zA-Z]
+//digit [0-9]
+//operator1 +|-|*|\
+//operator2 
+//string1 ^".*"$
+//string2 ^'.*'$
+//identifier1 ^_
+//integer 
+//decimal 
