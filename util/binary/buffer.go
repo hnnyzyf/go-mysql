570a35431d864b588bdee420135d926d7713360d
hnnyzyf <hnnyzyf2013@163.com>
2018-08-05 19:09:59 +0800

    modify bianry and add error checking

---
+++ util/binary/buffer.go
@@ -0,0 +1,441 @@
+package binary
+
+import (
+	"github.com/juju/errors"
+)
+
+var errTooSmall = errors.Errorf("binary:Payload is too small!")
+var errEOF = errors.Errorf("binary:EOF")
+
+//ReadInt1 reads 1 bytes
+func ReadInt1(buffer []byte) (uint8, error) {
+	if len(buffer) < 1 {
+		return 0, errTooSmall
+	}
+	return uint8(buffer[0]), nil
+}
+
+//ReadInt2 reads 2 bytes
+func ReadInt2(buffer []byte) (uint16, error) {
+	if len(buffer) < 2 {
+		return 0, errTooSmall
+	}
+	b := uint16(buffer[0])
+	var i uint8 = 1
+	for i < 2 {
+		b = b | uint16(buffer[i])<<(i*8)
+		i++
+	}
+	return b, nil
+}
+
+//ReadInt3 reads 3 bytes
+func ReadInt3(buffer []byte) (uint32, error) {
+	if len(buffer) < 3 {
+		return 0, errTooSmall
+	}
+	b := uint32(buffer[0])
+	var i uint8 = 1
+	for i < 3 {
+		b = b | uint32(buffer[i])<<(i*8)
+		i++
+	}
+	return b, nil
+}
+
+//ReadInt4 reads 4 bytes
+func ReadInt4(buffer []byte) (uint32, error) {
+	if len(buffer) < 4 {
+		return 0, errTooSmall
+	}
+	b := uint32(buffer[0])
+	var i uint8 = 1
+	for i < 4 {
+		b = b | uint32(buffer[i])<<(i*8)
+		i++
+	}
+	return b, nil
+}
+
+//ReadInt6 reads 6 bytes
+func ReadInt6(buffer []byte) (uint64, error) {
+	if len(buffer) < 6 {
+		return 0, errTooSmall
+	}
+	b := uint64(buffer[0])
+	var i uint8 = 1
+	for i < 6 {
+		b = b | uint64(buffer[i])<<(i*8)
+		i++
+	}
+	return b, nil
+}
+
+//ReadInt8 reads 8 bytes
+func ReadInt8(buffer []byte) (uint64, error) {
+	if len(buffer) < 8 {
+		return 0, errTooSmall
+	}
+	b := uint64(buffer[0])
+	var i uint8 = 1
+	for i < 8 {
+		b = b | uint64(buffer[i])<<(i*8)
+		i++
+	}
+	return b, nil
+}
+
+//WriteInt1 writes 1 byte into buffer
+func WriteInt1(b []byte, val uint8) error {
+	if len(b) < 1 {
+		return errTooSmall
+	}
+	b[0] = val
+	return nil
+}
+
+//WriteInt2 writes 2 bytes into buffer
+func WriteInt2(b []byte, val uint16) error {
+	if len(b) < 2 {
+		return errTooSmall
+	}
+	b[0] = uint8(val)
+	b[1] = uint8(val >> 8)
+	return nil
+}
+
+//WriteInt3 writes 3 bytes into buffer
+func WriteInt3(b []byte, val uint32) error {
+	if len(b) < 3 {
+		return errTooSmall
+	}
+	b[0] = uint8(val)
+	b[1] = uint8(val >> 8)
+	b[2] = uint8(val >> 16)
+	return nil
+}
+
+//WriteInt4 writes 4 bytes into buffer
+func WriteInt4(b []byte, val uint32) error {
+	if len(b) < 4 {
+		return errTooSmall
+	}
+	b[0] = uint8(val)
+	b[1] = uint8(val >> 8)
+	b[2] = uint8(val >> 16)
+	b[3] = uint8(val >> 24)
+	return nil
+
+}
+
+//WriteInt6 writes 6 bytes into buffer
+func WriteInt6(b []byte, val uint64) error {
+	if len(b) < 6 {
+		return errTooSmall
+	}
+	b[0] = uint8(val)
+	b[1] = uint8(val >> 8)
+	b[2] = uint8(val >> 16)
+	b[3] = uint8(val >> 24)
+	b[4] = uint8(val >> 32)
+	b[5] = uint8(val >> 40)
+	return nil
+}
+
+//WriteInt8 writes 8 bytes into buffer
+func WriteInt8(b []byte, val uint64) error {
+	if len(b) < 8 {
+		return errTooSmall
+	}
+	b[0] = uint8(val)
+	b[1] = uint8(val >> 8)
+	b[2] = uint8(val >> 16)
+	b[3] = uint8(val >> 24)
+	b[4] = uint8(val >> 32)
+	b[5] = uint8(val >> 40)
+	b[6] = uint8(val >> 48)
+	b[7] = uint8(val >> 56)
+	return nil
+}
+
+//StoreInt1 Store 1 byte into buffer
+func StoreInt1(val uint8) []byte {
+	b := make([]byte, 1)
+	b[0] = val
+	return b
+}
+
+//StoreInt2 Store 2 bytes into buffer
+func StoreInt2(val uint16) []byte {
+	b := make([]byte, 2)
+	b[0] = uint8(val)
+	b[1] = uint8(val >> 8)
+	return b
+}
+
+//StoreInt3 Store 3 bytes into buffer
+func StoreInt3(val uint32) []byte {
+	b := make([]byte, 3)
+	b[0] = uint8(val)
+	b[1] = uint8(val >> 8)
+	b[2] = uint8(val >> 16)
+	return b
+}
+
+//StoreInt4 Store 4 bytes into buffer
+func StoreInt4(val uint32) []byte {
+	b := make([]byte, 4)
+	b[0] = uint8(val)
+	b[1] = uint8(val >> 8)
+	b[2] = uint8(val >> 16)
+	b[3] = uint8(val >> 24)
+	return b
+}
+
+//StoreInt6 Store 6 bytes into buffer
+func StoreInt6(val uint64) []byte {
+	b := make([]byte, 6)
+	b[0] = uint8(val)
+	b[1] = uint8(val >> 8)
+	b[2] = uint8(val >> 16)
+	b[3] = uint8(val >> 24)
+	b[4] = uint8(val >> 32)
+	b[5] = uint8(val >> 40)
+	return b
+}
+
+//StoreInt8 Store 8 bytes into buffer
+func StoreInt8(val uint64) []byte {
+	b := make([]byte, 8)
+	b[0] = uint8(val)
+	b[1] = uint8(val >> 8)
+	b[2] = uint8(val >> 16)
+	b[3] = uint8(val >> 24)
+	b[4] = uint8(val >> 32)
+	b[5] = uint8(val >> 40)
+	b[6] = uint8(val >> 48)
+	b[7] = uint8(val >> 56)
+	return b
+}
+
+//Buffer represent a buffer in a packet
+type Buffer struct {
+	b   []byte
+	off int
+}
+
+func NewBuffer(b []byte) *Buffer {
+	return &Buffer{
+		b:   b,
+		off: 0,
+	}
+}
+
+//Read implement the io.Reader interface
+func (p *Buffer) Read(b []byte) (int, error) {
+	if len(p.b)-p.off < len(b) {
+		return -1, errTooSmall
+	}
+	n := copy(b, p.b[p.off:])
+	p.off += n
+	return n, nil
+}
+
+//Write implement the io.Write interface
+func (p *Buffer) Write(b []byte) (int, error) {
+	if len(p.b)-p.off < len(b) {
+		return -1, errTooSmall
+	}
+
+	return copy(p.b[p.off:], b), nil
+}
+
+//IsEOf return
+func (p *Buffer) IsEOF() bool {
+	return p.off >= len(p.b)
+}
+
+func (p *Buffer) Bytes() []byte {
+	return p.b
+}
+
+//IsEOf return
+func (p *Buffer) Len() int {
+	return len(p.b)
+}
+
+//ReadInt1 reads 1 bytes
+func (p *Buffer) ReadInt1() (uint8, error) {
+	buffer := p.b[p.off:]
+	p.off += 1
+	return ReadInt1(buffer)
+}
+
+//ReadInt2 reads 2 bytes
+func (p *Buffer) ReadInt2() (uint16, error) {
+	buffer := p.b[p.off:]
+	p.off += 2
+	return ReadInt2(buffer)
+}
+
+//ReadInt3 reads 3 bytes
+func (p *Buffer) ReadInt3() (uint32, error) {
+	buffer := p.b[p.off:]
+	p.off += 3
+	return ReadInt3(buffer)
+}
+
+//ReadInt4 reads 4 bytes
+func (p *Buffer) ReadInt4() (uint32, error) {
+	buffer := p.b[p.off:]
+	p.off += 4
+	return ReadInt4(buffer)
+}
+
+//ReadInt6 reads 6 bytes
+func (p *Buffer) ReadInt6() (uint64, error) {
+	buffer := p.b[p.off:]
+	p.off += 6
+	return ReadInt6(buffer)
+}
+
+//ReadInt8 reads 8 bytes
+func (p *Buffer) ReadInt8() (uint64, error) {
+	buffer := p.b[p.off:]
+	p.off += 8
+	return ReadInt8(buffer)
+}
+
+//ReadZero reads length bytes
+func (p *Buffer) ReadZero(l int) error {
+	if p.off+l > len(p.b) {
+		return errTooSmall
+	}
+	p.off += l
+	return nil
+}
+
+//ReadLengthEncodedInteger read undetermined length integer
+func (p *Buffer) ReadLengthEncodedInteger() (int, uint64, error) {
+
+	if p.IsEOF() {
+		return -1, 0, errEOF
+	}
+	switch i := p.b[p.off]; {
+	case i < 0xfb:
+		val, err := p.ReadInt1()
+		return 1, uint64(val), err
+	case i == 0xfc:
+		val, err := p.ReadInt2()
+		return 2, uint64(val), err
+	case i == 0xfd:
+		val, err := p.ReadInt3()
+		return 3, uint64(val), err
+	case i == 0xfe:
+		val, err := p.ReadInt8()
+		return 8, val, err
+	default:
+		panic("Invaid LengthEncodedInteger flag!")
+	}
+
+	return -1, 0, nil
+}
+
+//ReadStringWithNull reads a string terminated with null(0x00)
+func (p *Buffer) ReadStringWithNull() ([]byte, error) {
+	if p.IsEOF() {
+		return nil, errEOF
+	}
+	for off := p.off; off < len(p.b); off++ {
+		if p.b[off] == 0x00 {
+			//return not include 0x00
+			b := p.b[p.off:off]
+			p.off = off + 1
+			return b, nil
+		}
+	}
+	//return all bytes
+	return p.b[p.off:], nil
+}
+
+//ReadStringWithEOF reads a string util EOF
+func (p *Buffer) ReadStringWithEOF() ([]byte, error) {
+	if p.IsEOF() {
+		return nil, errEOF
+	}
+	return p.b[p.off:], nil
+}
+
+//ReadStringWithFixLen reads a string  with fixed length
+func (p *Buffer) ReadStringWithFixLen(l int) ([]byte, error) {
+	if p.off+l > len(p.b) {
+		return nil, errTooSmall
+	}
+	b := p.b[p.off : p.off+l]
+	p.off += l
+	return b, nil
+}
+
+//WriteInt1 write 1 byte into buffer
+func (p *Buffer) WriteInt1(val uint8) error {
+	buffer := p.b[p.off:]
+	p.off += 1
+	return WriteInt1(buffer, val)
+}
+
+func (p *Buffer) WriteInt2(val uint16) error {
+	buffer := p.b[p.off:]
+	p.off += 2
+	return WriteInt2(buffer, val)
+}
+
+func (p *Buffer) WriteInt3(val uint32) error {
+	buffer := p.b[p.off:]
+	p.off += 3
+	return WriteInt3(buffer, val)
+}
+
+func (p *Buffer) WriteInt4(val uint32) error {
+	buffer := p.b[p.off:]
+	p.off += 4
+	return WriteInt4(buffer, val)
+}
+
+func (p *Buffer) WriteInt6(val uint64) error {
+	buffer := p.b[p.off:]
+	p.off += 6
+	return WriteInt6(buffer, val)
+}
+
+func (p *Buffer) WriteInt8(val uint64) error {
+	buffer := p.b[p.off:]
+	p.off += 8
+	return WriteInt8(buffer, val)
+}
+
+func (p *Buffer) WriteZero(l int) error {
+	if p.off+l > len(p.b) {
+		return errTooSmall
+	}
+	p.off += l
+	return nil
+}
+
+func (p *Buffer) WriteStringWithNull(str []byte) error {
+	if p.off+len(str)+1 > len(p.b) {
+		return errTooSmall
+	}
+	_ = copy(p.b[p.off:], str)
+	p.off += len(str)
+	p.WriteZero(1)
+	return nil
+}
+
+func (p *Buffer) WriteStringWithFixLen(str []byte) error {
+	if p.off+len(str)+1 > len(p.b) {
+		return errTooSmall
+	}
+	_ = copy(p.b[p.off:], str)
+	p.off += len(str)
+	return nil
+}
---
+++ util/binary/buffer_test.go
@@ -0,0 +1,109 @@
+package binary
+
+import (
+	//"fmt"
+	"testing"
+
+	. "gopkg.in/check.v1"
+)
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) { TestingT(t) }
+
+type MyPayloadSuite struct{}
+
+var _ = Suite(&MyPayloadSuite{})
+
+func (s *MyPayloadSuite) TestWriteInteger(c *C) {
+	b := make([]byte, 24)
+	p := NewBuffer(b)
+
+	err := p.WriteInt1(0x01)
+	c.Assert(err, Equals, nil)
+
+	err = p.WriteInt2(0x0203)
+	c.Assert(err, Equals, nil)
+
+	err = p.WriteInt3(0x040506)
+	c.Assert(err, Equals, nil)
+
+	err = p.WriteInt4(0x0708090A)
+	c.Assert(err, Equals, nil)
+
+	err = p.WriteInt6(0x123456789ABC)
+	c.Assert(err, Equals, nil)
+
+	err = p.WriteInt8(0x1F2F3F4F5F6F7F8F)
+	c.Assert(err, Equals, nil)
+
+	res := []byte{0x01, 0x03, 0x02, 0x06, 0x05, 0x04, 0x0A, 0x09, 0x08, 0x07, 0xBC, 0x9A, 0x78, 0x56, 0x34, 0x12}
+
+	for i := range res {
+		c.Assert(res[i], Equals, b[i])
+	}
+
+}
+
+func (s *MyPayloadSuite) TestReadInteger(c *C) {
+	b := []byte{0x01, 0x03, 0x02, 0x06, 0x05, 0x04, 0x0A, 0x09, 0x08, 0x07, 0xBC, 0x9A, 0x78, 0x56, 0x34, 0x12,
+		0x8F, 0x7F, 0x6F, 0x5F, 0x4F, 0x3F, 0x2F, 0x1F}
+
+	p := NewBuffer(b)
+
+	res1, err := p.ReadInt1()
+	c.Assert(err, Equals, nil)
+	c.Assert(res1, Equals, uint8(0x01))
+
+	res2, err := p.ReadInt2()
+	c.Assert(err, Equals, nil)
+
+	c.Assert(res2, Equals, uint16(0x0203))
+
+	res3, err := p.ReadInt3()
+	c.Assert(err, Equals, nil)
+
+	c.Assert(res3, Equals, uint32(0x040506))
+
+	res4, err := p.ReadInt4()
+	c.Assert(err, Equals, nil)
+
+	c.Assert(res4, Equals, uint32(0x0708090A))
+
+	res5, err := p.ReadInt6()
+	c.Assert(err, Equals, nil)
+
+	c.Assert(res5, Equals, uint64(0x123456789ABC))
+
+	res6, err := p.ReadInt8()
+	c.Assert(err, Equals, nil)
+
+	c.Assert(res6, Equals, uint64(0x1F2F3F4F5F6F7F8F))
+
+}
+
+func (s *MyPayloadSuite) TestWriteStringWithNull(c *C) {
+	st := []byte("我们做个测试")
+	b := make([]byte, len(st)+1)
+	p := NewBuffer(b)
+
+	err := p.WriteStringWithNull(st)
+	c.Assert(err, Equals, nil)
+
+	res := []byte{230, 136, 145, 228, 187, 172, 229, 129, 154, 228, 184, 170, 230, 181, 139, 232, 175, 149, 0}
+
+	for i := range res {
+		c.Assert(res[i], Equals, b[i])
+	}
+
+}
+func (s *MyPayloadSuite) TestReadStringWithNull(c *C) {
+	b := []byte{230, 136, 145, 228, 187, 172, 229, 129, 154, 228, 184, 170, 230, 181, 139, 232, 175, 149, 0}
+	p := NewBuffer(b)
+
+	res := "我们做个测试"
+	st, err := p.ReadStringWithNull()
+	c.Assert(err, Equals, nil)
+
+	c.Assert(string(st), Equals, res)
+
+}
