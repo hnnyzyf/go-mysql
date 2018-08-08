8506d3d721252682fb44eca9ffa71a29c8fc571d
hnnyzyf <hnnyzyf2013@163.com>
2018-08-09 02:05:23 +0800

    add test method

--- client/util.go
+++ client/util.go
@@ -6,30 +6,40 @@ import (
 
 //testProtocolVersion tests whether  the server supprot v10 protocol
 func testProtocolVersion(version uint8) bool {
-	return version != 0x0A
+	return version == 0x0A
 }
 
 //testPluginAuth tests whether capability contains CLIENT_PLUGIN_AUTH
 func testPluginAuth(capability uint32) bool {
-	return capability&protocol.CLIENT_PLUGIN_AUTH == 0
+	return capability&protocol.CLIENT_PLUGIN_AUTH != 0
 }
 
 //testProtocol41 tests whether capability contains CLIENT_PROTOCOL_41
 func testProtocol41(capability uint32) bool {
-	return capability&protocol.CLIENT_PROTOCOL_41 == 0
+	return capability&protocol.CLIENT_PROTOCOL_41 != 0
 }
 
 //testSecureConnection tests whether capability contains CLIENT_SECURE_CONNECTION
 func testSecureConnection(capability uint32) bool {
-	return capability&protocol.CLIENT_SECURE_CONNECTION == 0
+	return capability&protocol.CLIENT_SECURE_CONNECTION != 0
 }
 
 //testSSL tests whether capability contains CLIENT_SSL
 func testSSL(capability uint32) bool {
-	return capability&protocol.CLIENT_SSL == 0
+	return capability&protocol.CLIENT_SSL != 0
 }
 
 //testLenencData tests whether capability contains CLIENT_PLUGIN_AUTH_LENENC_CLIENT_DATA
-func testLenencData(capability uint32) bool{
-	return capability&protocol.CLIENT_PLUGIN_AUTH_LENENC_CLIENT_DATA ==0
+func testLenencData(capability uint32) bool {
+	return capability&protocol.CLIENT_PLUGIN_AUTH_LENENC_CLIENT_DATA != 0
+}
+
+//testConnectWithDB tests whether capability contains CLIENT_CONNECT_WITH_DB
+func testConnectWithDB(capability uint32) bool {
+	return capability&protocol.CLIENT_CONNECT_WITH_DB != 0
+}
+
+//testConnectAttrs tests whether capability contains CLIENT_CONNECT_ATTRS
+func testConnectAttrs(capability uint32) bool {
+	return capability&protocol.CLIENT_CONNECT_ATTRS != 0
 }
--- client/util_test.go
+++ client/util_test.go
@@ -1,1 +1,48 @@
 package client
+
+import (
+	. "gopkg.in/check.v1"
+
+	"github.com/hnnyzyf/go-mysql/protocol"
+)
+
+type MytestSuite struct{}
+
+var _ = Suite(&MytestSuite{})
+
+func (s *MytestSuite) TestVersion(c *C) {
+	data := []struct {
+		version uint8
+		res     bool
+	}{
+		{0x01, false},
+		{0x0a, true},
+		{0xff, false},
+	}
+
+	for i := range data {
+		c.Assert(testProtocolVersion(data[i].version), Equals, data[i].res)
+	}
+}
+
+type testfunction func(uint32) bool
+
+func (s *MytestSuite) TestClientConst(c *C) {
+	data := []struct {
+		f   testfunction
+		c   uint32
+		res bool
+	}{
+		{testPluginAuth, protocol.CLIENT_PLUGIN_AUTH, true},
+		{testProtocol41, protocol.CLIENT_PROTOCOL_41, true},
+		{testSecureConnection, protocol.CLIENT_SECURE_CONNECTION, true},
+		{testSSL, protocol.CLIENT_SSL, true},
+		{testLenencData, protocol.CLIENT_PLUGIN_AUTH_LENENC_CLIENT_DATA, true},
+		{testConnectWithDB, protocol.CLIENT_CONNECT_WITH_DB, true},
+		{testConnectAttrs, protocol.CLIENT_CONNECT_ATTRS, true},
+	}
+
+	for i := range data {
+		c.Assert(data[i].f(data[i].c), Equals, data[i].res)
+	}
+}
